bd6ee9c7c69799da696db34dbc77e90a20cf03a1
Alexander <algashev@mail.ru>
2020-11-21 22:10:43 +0300

    Remove CoreDataStack in ViewController

--- Notes/AppDelegate.swift
+++ Notes/AppDelegate.swift
@@ -13,10 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let coreDataStack = try! CoreDataStack(modelName: "Notes") { error in
-            print("Core Data Stack is ready")
-        }
-        print(coreDataStack.context)
         
         return true
     }
--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private var coreDataStack: CoreDataStack!
+    
+    // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.initializeCoreDataStack()
     }
 
+    private func initializeCoreDataStack() {
+        do {
+            self.coreDataStack = try CoreDataStack(modelName: "Notes") { error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+                
+                print("Core Data Stack is ready")
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
