9e415080f92a2c8b9c0cf525b7a97b43428dcd32
Alexander Algashev <algashev@mail.ru>
2020-11-22 12:44:52 +0300

    Add AddNoteViewController

---
+++ Notes/Controllers/AddNoteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddNoteViewController.swift
+//  Notes
+//
+//  Created by Александр Алгашев on 22.11.2020.
+//
+
+import UIKit
+
+class AddNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
